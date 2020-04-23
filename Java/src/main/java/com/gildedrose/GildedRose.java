a168388cc57283504b22c3bc3a4f8607d7366801
bramallard <bram.allard@refleqt.be>
2020-04-23 14:26:33 +0200

    Extract private methods for readability

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -12,53 +12,63 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
-                if (item.quality > 0) {
-                    if (!item.name.equals(SULFURAS)) {
-                        item.quality = item.quality - 1;
+            adjustItemQualityByItemName(item);
+            decreaseSellInByOne(item);
+            adjustItemQualityBelowSellIn(item);
+        }
+    }
+
+    private void adjustItemQualityBelowSellIn(Item item) {
+        if (item.sellIn < 0) {
+            if (!item.name.equals(AGED_BRIE)) {
+                if (!item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+                        decreaseItemQualityByAmount(item, 1);
                     }
+                } else {
+                    item.quality = 0;
                 }
             } else {
                 if (item.quality < 50) {
-                    item.quality = item.quality + 1;
+                    increaseItemQualityByAmount(item, 1);
+                }
+            }
+        }
+    }
 
-                    if (item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+    private void adjustItemQualityByItemName(Item item) {
+        if (!item.name.equals(AGED_BRIE) && !item.name.equals(BACKSTAGE_PASSES)) {
+            decreaseItemQualityByAmount(item, 1);
+        } else {
+            if (item.quality < 50) {
+                increaseItemQualityByAmount(item, 1);
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
+                if (item.name.equals(BACKSTAGE_PASSES)) {
+                    if (item.sellIn < 11 && item.quality < 50) {
+                        increaseItemQualityByAmount(item, 1);
+                    }
+
+                    if (item.sellIn < 6 && item.quality < 50) {
+                        increaseItemQualityByAmount(item, 1);
                     }
                 }
             }
+        }
+    }
 
-            if (!item.name.equals(SULFURAS)) {
-                item.sellIn = item.sellIn - 1;
-            }
+    private void decreaseSellInByOne(Item item) {
+        if (!item.name.equals(SULFURAS)) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
 
-            if (item.sellIn < 0) {
-                if (!item.name.equals(AGED_BRIE)) {
-                    if (!item.name.equals(BACKSTAGE_PASSES)) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals(SULFURAS)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = item.quality - item.quality;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+    private void increaseItemQualityByAmount(Item item, int amountToIncreaseBy) {
+        item.quality = item.quality + amountToIncreaseBy;
+    }
+
+    private void decreaseItemQualityByAmount(Item item, int amountToDecreaseBy) {
+        if (item.quality > 0 && !item.name.equals(SULFURAS)) {
+            item.quality = item.quality - amountToDecreaseBy;
         }
     }
 }
